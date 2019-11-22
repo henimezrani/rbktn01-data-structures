92c4bb52824f742eb1f7bff6f15807983d50da75
henimezrani <henimezrani@gmail.com>
2019-11-22 19:39:39 +0100

    Sprint 2 solved

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,75 @@
 var BinarySearchTree = function(value) {
+	var node = {};
+	node.value = value;
+	node.left;
+	node.right;
+	Object.assign(node, methods);
+
+	return node;
 };
 
+var methods = {
+	insert: function(value, currentNode) {
+		var currentNode = currentNode || this;
+
+		if(currentNode.value > value ){
+			if(currentNode.left) {
+				this.insert(value, currentNode.left);
+			}else{
+				currentNode.left = new BinarySearchTree(value);
+			}
+		} else {
+			if(currentNode.right) {
+				this.insert(value, currentNode.right);
+			}else{
+				currentNode.right = new BinarySearchTree(value);
+			}
+		}
+
+	},
+	contains: function(value, currentNode, result) {
+		var currentNode = currentNode || this;
+		var result = result || false;
+
+		if(!result){
+			if(value === currentNode.value){
+			 result = true;
+			 return result;
+			}
+
+			if(currentNode.value > value ){
+				if(currentNode.left){
+					return this.contains(value, currentNode.left, result);
+				}
+			} 
+			else {
+				if(currentNode.right) {
+					return this.contains(value, currentNode.right, result);
+				}
+			}
+		}
+
+		return result;
+	},
+	depthFirstLog: function(func, currentNode) {
+		if(currentNode === undefined){
+			var currentNode = this;
+			func(currentNode.value);
+			this.depthFirstLog(func, currentNode.left);
+			this.depthFirstLog(func, currentNode.right);
+		}
+		else{
+			var currentNode = currentNode;
+				func(currentNode.value);
+			if(currentNode.right) {
+				this.depthFirstLog(func, currentNode.right);
+			}
+			if(currentNode.left) {
+				this.depthFirstLog(func, currentNode.left);
+			}
+		}
+	}
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,16 +7,34 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = [];
+  var tuple = [k,v];
+
+  if(this._storage.get(index) !== undefined){
+		bucket = this._storage.get(index);
+  }
+  
+  bucket.push(tuple);
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var element = this._storage.get(index);
+
+  if(this._storage.get(index) !== undefined){
+	  for (var i = element.length - 1; i >= 0; i--) {
+	  	if (element[i][0] === k) {
+	  		return element[i][1]
+	  	}
+	  }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index,undefined);
 };
-
 
 
 /*
