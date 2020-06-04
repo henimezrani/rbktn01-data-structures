6e16bb5d8f3818a68e5da4a95e48054e09628bcc
henimezrani <henimezrani@gmail.com>
2020-06-04 14:19:07 +0100

    graph using 2D matrix

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,80 +1,60 @@
-
-
 // Instantiate a new graph
-var Graph = function(value) {
-	this.graph = Array();
+var Graph = function () {
+  this.graph = new Array();
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
-
-	if (this.graph[node] !== undefined){
-		this.graph[node][node] = 1;
-	} else {
-		var tmpLength = this.graph.length;
-
-		this.graph[node] = [];
-		this.graph[node][node] = 1;
-	}
+Graph.prototype.addNode = function (node) {
+  if (!this.graph[node]) {
+    this.graph[node] = [];
+  }
+  this.graph[node][node] = 1;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
-	return this.graph[node][node] === 1;
+Graph.prototype.contains = function (node) {
+  return !!this.graph[node][node];
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
-	this.graph[node][node] = undefined;
-	for (var i = 0; i < this.graph.length; i++) {
-		if (this.graph[i]){
-			this.graph[i][node] = undefined;
-			this.graph[node][i] = undefined;
-		}
-	}
+Graph.prototype.removeNode = function (node) {
+  this.graph[node][node] = undefined;
+  for (var i = 0; i < this.graph.length; i++) {
+    if (this.graph[i]) {
+      this.graph[i][node] = undefined;
+      this.graph[node][i] = undefined;
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
-	return Boolean(this.graph[fromNode][toNode]);
+Graph.prototype.hasEdge = function (fromNode, toNode) {
+  return !!this.graph[fromNode][toNode];
 };
 
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
-		if(this.graph[toNode] === undefined || this.graph[fromNode] === undefined){
-		this.graph[fromNode] = [];
-		this.graph[fromNode][toNode] = 1;
-		this.graph[toNode] = [];
-		this.graph[toNode][fromNode] = 1;
-	}else{
-		this.graph[fromNode][toNode] = 1;
-		this.graph[toNode][fromNode] = 1;
-	}
+Graph.prototype.addEdge = function (fromNode, toNode) {
+  if (!this.graph[toNode] || !this.graph[fromNode]) {
+    this.graph[fromNode] = [];
+    this.graph[toNode] = [];
+  }
+  this.graph[fromNode][toNode] = 1;
+  this.graph[toNode][fromNode] = 1;
 };
 
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
-	if(this.graph[toNode] === undefined || this.graph[fromNode] === undefined){
-		this.graph[fromNode] = [];
-		this.graph[fromNode][toNode] = undefined;
-		this.graph[toNode] = [];
-		this.graph[toNode][fromNode] = undefined;
-	}else{
-		this.graph[fromNode][toNode] = undefined;
-		this.graph[toNode][fromNode] = undefined;
-	}
-
+Graph.prototype.removeEdge = function (fromNode, toNode) {
+  if (!this.graph[toNode] || !this.graph[fromNode]) {
+    this.graph[fromNode] = [];
+    this.graph[toNode] = [];
+  }
+  this.graph[fromNode][toNode] = undefined;
+  this.graph[toNode][fromNode] = undefined;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) {
-	for (var i = 0; i < this.graph.length; i++) {
-		cb(i)
-	}
+Graph.prototype.forEachNode = function (cb) {
+  for (var i = 0; i < this.graph.length; i++) {
+    cb(i);
+  }
 };
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
