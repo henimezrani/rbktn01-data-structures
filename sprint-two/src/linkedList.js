a24cc4225ea92fe4374bf426f32f6f5835b60269
henimezrani <henimezrani@gmail.com>
2020-06-04 14:22:35 +0100

    Linked list refactored

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,42 +1,39 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
-    var tmp = Node(value);
-    if(list.head === null && list.tail === null){
+  list.addToTail = function (value) {
+    var tmp = new Node(value);
+    if (list.head === null && list.tail === null) {
       list.head = tmp;
-      list.tail = tmp;
-    }else{
+    } else {
       list.tail.next = tmp;
-      list.tail = tmp;
     }
+    list.tail = tmp;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
     var tmp = list.head.value;
     list.head = list.head.next;
-
     return tmp;
   };
 
-  list.contains = function(target) {
-    var currNode = list.head;
-
-    while(currNode){
-      if(currNode.value === target){
+  list.contains = function (target) {
+    var currentNode = list.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
         return true;
       }
-      currNode = currNode.next;
+      currentNode = currentNode.next;
     }
-    return false
+    return false;
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
